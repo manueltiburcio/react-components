478802166188098b574cfeb5c08a6e9e738947a8
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-08-01 14:17:25 -0700

    Implemented hover

--- app.js
+++ app.js
@@ -1,74 +1,63 @@
-// TODO
-
-// A class component can be defined as an ES6 class
-// that extends the base Component class included in the React library
 class GroceryListItem extends React.Component {
-
-  // A `constructor` method is expected on all ES6 classes
-  // When React instantiates the component,
-  // it will pass `props` to the constructor
-
   constructor(props) {
     super(props);
 
     this.state = {
       done: false,
       hover: false
-    };
+    }
   }
 
   onListItemClick() {
     this.setState({
       done: !this.state.done
-    });
+    })
   }
 
-  onListItemMouseEnter() {
-    this.setState({
-      hover: true
-    });
+  onMouseEnter() {
+    this.setState({hover: true})
   }
 
-  onListItemMouseLeave() {
-    this.setState({
-      hover: false
-    });
+  onMouseLeave() {
+    this.setState({hover: false})
   }
 
-  // Every class component must have a `render` method
-  // Stateless functional components are pretty much just this method
+
   render() {
-
     var style = {
       textDecoration: this.state.done ? 'line-through' : 'none',
       fontWeight: this.state.hover ? 'bold' : 'normal',
-    };
+    }
 
     return (
-      <li style={style}
-      onClick={this.onListItemClick.bind(this)}
-      onListItemMouseEnter={this.onListItemMouseEnter.bind(this)}
-      onListItemMouseLeave={this.onListItemMouseLeave.bind(this)}>{this.props.todo}</li>
-    );
+      <li style={style} onClick={this.onListItemClick.bind(this)}
+      onMouseEnter={this.onMouseEnter.bind(this)}
+      onMouseLeave={this.onMouseLeave.bind(this)}>{this.props.todo}</li>
+    )
   }
+
 }
 
-var GroceryList = (props) => (
+var GroceryList = (props) => {
+
+
+
+  return (
   <ul>
-    {props.todos.map(todo =>
-      <GroceryListItem todo={todo} />
-    )}
+    {props.todos.map(todo => <GroceryListItem todo={todo}/>)}
   </ul>
-);
+  );
+
+}
+
+
 
 
 var App = () => (
   <div>
-    <h2>My Grocery List</h2>
-    <GroceryList todos={['Milk', 'Cereal', 'Bread', 'Potato', 'Ham']}></GroceryList>
+    <h2>GroceryList</h2>
+    <GroceryList todos={['Milk', 'Cookie', 'Cereal']}/>
   </div>
 );
-
-
 
 ReactDOM.render(<App />, document.getElementById("app"));
